52135ee03ad7dd7e07591c1e5a625611ba201a01
mackos1112 <m.lukaszczyk1112@gmail.com>
2025-04-26 11:28:31 +0200

    change to hexes and adding a bit of statistics, added size_t making it a bit newer

---
+++ cmake-build-debug/CMakeFiles/GameOfLife.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameOfLife.dir/Game_Of_Life_v1.cpp.obj"
+  "CMakeFiles/GameOfLife.dir/Game_Of_Life_v1.cpp.obj.d"
+  "GameOfLife.exe"
+  "GameOfLife.exe.manifest"
+  "GameOfLife.pdb"
+  "libGameOfLife.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameOfLife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
